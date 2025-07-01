8b6269d4f5b2b25dea69aea084baec7a4cb8a97f
naveenv0906 <naveenv0906@gmail.com>
2025-07-01 17:43:21 +0530

    prisma timeout changed to 10s

--- project/server/src/prisma/prisma.service.ts
+++ project/server/src/prisma/prisma.service.ts
@@ -2,7 +2,20 @@ import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    super({
+      // 👇 Set transaction timeout (10 seconds globally)
+      transactionOptions: {
+        maxWait: 10000, // Max time to wait for a transaction (10s)
+        timeout: 10000, // Max time the transaction can run (10s)
+      },
+    });
+  }
+  
   async onModuleInit() {
     await this.$connect();
   }
